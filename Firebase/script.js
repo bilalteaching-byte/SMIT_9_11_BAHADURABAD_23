dbbd71322014996a67594fe3d0ab86e5726b1e8c
Mohammad Saad Ali <bilalteaching@gmail.com>
2026-08-29 11:00:27 +0500

    [UPDATE] learned firebase authentication

--- Firebase/script.js
+++ Firebase/script.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-analytics.js";
+
+//firestore
 import {
   getFirestore,
   collection,
@@ -7,10 +9,32 @@ import {
   getDocs,
 } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
 
+//auth
+
+import {
+  getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword
+  , onAuthStateChanged , signOut 
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
+
+
+
 let addBtn = document.getElementById("addPost");
 let title = document.getElementById("title");
 let description = document.getElementById("description");
 let allPosts = document.getElementById("allPosts");
+let name = document.getElementById("name");
+let register_email = document.getElementById("register_email");
+let register_password = document.getElementById("register_password");
+let registerBtn = document.getElementById("registerBtn");
+let login_email = document.getElementById("login_email");
+let login_password = document.getElementById("login_password");
+let loginBtn = document.getElementById("loginBtn");
+let auth_container = document.getElementById("auth_container");
+let dashboard_container = document.getElementById("dashboard_container");
+let welcomeMsg = document.getElementById("welcomeMsg");
+let signoutBtn = document.getElementById("signoutBtn");
+
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyCo96ak3VFU5_jZPA6qon_VGdK2W9kwqd0",
@@ -27,9 +51,95 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 const db = getFirestore(app);
+const auth = getAuth(app);
 
 //collection , create ref for collection
 //addDoc , it takes two Params , one is reference to collection , second is document to be added
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    console.log("User is logged in", user)
+    const uid = user.uid;
+    auth_container.style.display = "none"
+    dashboard_container.style.display = "block"
+    welcomeMsg.innerHTML = "Hello " + user.email
+    // ...
+  } else {
+    console.log("User is not logged in")
+    auth_container.style.display = "block"
+    dashboard_container.style.display = "none"
+    // User is signed out
+    // ...
+  }
+});
+
+
+let registerUser = () => {
+  registerBtn.disabled = true
+  createUserWithEmailAndPassword(auth, register_email.value, register_password.value)
+    .then((userCredential) => {
+      // Signed up 
+      const user = userCredential.user;
+      Toastify({
+        text: "User Created Successfully",
+      }).showToast();
+      registerBtn.disabled = false
+      name.value = ""
+      register_email.value = ""
+      register_password.value = ""
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      registerBtn.disabled = false
+      Toastify({
+        text: errorMessage,
+        style: {
+          background: "red",
+          color: "white"
+        }
+      }).showToast();
+      // ..
+    });
+}
+
+let signinUser = () => {
+  loginBtn.disabled = true
+  signInWithEmailAndPassword(auth, login_email.value, login_password.value)
+    .then((userCredential) => {
+      // Signed in 
+      Toastify({
+        text: "User Signin Successfully",
+      }).showToast();
+      const user = userCredential.user;
+      console.log("user=>", user)
+      loginBtn.disabled = false
+
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      Toastify({
+        text: errorMessage,
+        style: {
+          background: "red",
+          color: "white"
+        }
+      }).showToast();
+      loginBtn.disabled = false
+
+    });
+}
+
+let logoutUser = () => {
+  signOut(auth).then(() => {
+    // Sign-out successful.
+  }).catch((error) => {
+    // An error happened.
+  });
+}
 
 let addPostToDb = () => {
   addBtn.disabled = true;
@@ -68,3 +178,6 @@ let getPostsFromDB = () => {
 getPostsFromDB();
 
 addBtn.addEventListener("click", addPostToDb);
+registerBtn.addEventListener("click", registerUser);
+loginBtn.addEventListener("click", signinUser);
+signoutBtn.addEventListener("click", logoutUser)
